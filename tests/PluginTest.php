478cd1fbddf4a198b55c18d6afb3b02819eafed6
Jacob Emerick <jpemeric@gmail.com>
2015-09-11 09:04:38 -0700

    Adds setup of phaked things

--- tests/PluginTest.php
+++ tests/PluginTest.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * Phergie plugin for Use Bigstock API to search for and display images (https://github.com/shutterstock/phergie-irc-plugin-react-bigstock)
+ * Phergie plugin for Use Bigstock API to search for and display images
+ * (https://github.com/shutterstock/phergie-irc-plugin-react-bigstock)
  *
  * @link https://github.com/shutterstock/phergie-irc-plugin-react-bigstock for the canonical source repository
  * @copyright Copyright (c) 2015 Shutterstock, Inc. (http://www.bigstockphoto.com)
@@ -25,12 +26,31 @@ class PluginTest extends \PHPUnit_Framework_TestCase
 {
 
 
+    protected function setUp()
+    {
+        $this->event = Phake::mock('\Phergie\Irc\Event\UserEventInterface');
+        $this->queue = Phake::mock('\Phergie\Irc\Bot\React\EventQueueInterface');
+        $this->emitter = Phake::mock('\Evenement\EventEmitterInterface');
+        $this->logger = Phake::mock('\Psr\Log\LoggerInterface');
+        $this->plugin = $this->getPlugin();
+    }
+
+    protected function getPlugin(array $config = [])
+    {
+        $config['accountId'] = 'ACCOUNT';
+        $plugin = new Plugin($config);
+        $plugin->setEventEmitter($this->emitter);
+        $plugin->setLogger($this->logger);
+        return $plugin;
+    }
+
     /**
      * Tests that getSubscribedEvents() returns an array.
      */
     public function testGetSubscribedEvents()
     {
-        $plugin = new Plugin;
-        $this->assertInternalType('array', $plugin->getSubscribedEvents());
+        $this->assertInternalType('array', $this->plugin->getSubscribedEvents());
     }
+
 }
+
